998c26fd3c9ac8501681bf79ee08e08ffeedfc72
glody007 <glodymbutwile@gmail.com>
2023-04-09 17:22:20 +0200

    feat: Nav favorites component

---
+++ components/NavFavorites.js
@@ -0,0 +1,46 @@
+import { View, Text, FlatList, TouchableOpacity } from 'react-native'
+import React from 'react'
+import { Icon } from '@rneui/base'
+
+const favorites = [
+    {
+        id: '1',
+        location: 'Work',
+        icon: 'briefcase',
+        destination: 'Lubumbashi, gambela, CD'
+    },
+    {
+        id: '2',
+        location: 'Home',
+        icon: 'home',
+        destination: 'Lubumbashi, gambela, CD'
+    }
+]
+
+export default function NavFavorites() {
+  return (
+    <FlatList 
+        className="mt-4"
+        data={favorites}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => (
+            <TouchableOpacity
+                className="flex-row items-center mt-4 space-x-4"
+            >
+                <View className="bg-gray-200 rounded-full p-3">
+                    <Icon 
+                        name={item.icon}
+                        size={16}
+                        color='white' 
+                        type='ionicon' 
+                    />
+                </View>
+                <View>
+                    <Text className="text-lg font-bold">{item.location}</Text>
+                    <Text className="text-gray-500">{item.destination}</Text>
+                </View>
+            </TouchableOpacity>
+        )}
+    />
+  )
+}
